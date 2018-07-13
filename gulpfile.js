94bea09dd694ffc4faa11ea732adbb5cd70e275e
小倉 且也 <wm.iidx@gmail.com>
2018-07-13 16:48:41 +0900

    oguemon.jsの圧縮を自動で行うように設定（そしてoguemon.min.jsはgit管理しない）

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyjs = require("gulp-closurecompiler");
+
 gulp.task('sass', function() {
     gulp.src('./sass/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
 });
-gulp.task('default', ['sass'], function() {
+gulp.task('minifyjs', function() {
+    gulp.src('./js/oguemon.js')
+        .pipe(minifyjs({
+            compilerPath: 'node_modules/closurecompiler/compiler/closure-compiler-v20180610.jar',
+            fileName: 'oguemon.min.js'
+        }))
+        .pipe(gulp.dest('./js'));
+});
+gulp.task('default', ['sass', 'minifyjs'], function() {
     gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('./js/oguemon.js', ['minifyjs']);
 })
