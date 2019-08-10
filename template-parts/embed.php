9f4a6b972f88aa0862d80be3d0009aedf5d44d30
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2019-08-10 22:39:57 +0900

    embedをデザイン

--- template-parts/embed.php
+++ template-parts/embed.php
@@ -30,15 +30,36 @@ if ( ! headers_sent() ) {
 	?>
 </head>
 <body>
-<?
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		get_template_part( 'embed', 'content' );
-	endwhile;
-else :
-	get_template_part( 'embed', '404' );
-endif;
+<?php
+// 流し込む文字列
+$item_url = '';
+$title = '';
+$thum_tag = '';
+
+// 404チェック
+if (!have_posts()) {
+	$item_url = get_bloginfo('url');
+	$title = '申し訳ございません。<br>当該ページは見つかりませんでした。';
+	$thum_tag = '<img src="' . get_template_directory_uri() . '/img/404.svg">';
+} else {
+	// 記事情報を取得
+	the_post();
+
+	$item_url = get_permalink();
+	$title = get_the_title();
+
+	// サムネの有無に応じた処理分け
+	if (has_post_thumbnail()) {
+		$thum_tag = get_the_post_thumbnail();
+	} else {
+		$thum_tag = '<img src="' . get_template_directory_uri() . '/img/ogp.svg">';
+	}
+}
 ?>
+<a href="<?= $item_url ?>" target="_top" class="embed-item">
+	<div class="thumbnail"><?= $thum_tag ?></div>
+	<div class="title"><?= $title ?></div>
+	<div class="blogname"><?= get_bloginfo('name'); ?></div>
+</a>
 </body>
 </html>
