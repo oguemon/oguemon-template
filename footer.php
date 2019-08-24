d1cd41c333907d94eabf8a63fe72b3d964becd6f
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2019-08-24 12:55:34 +0900

    フッターのリデザイン（SNSアイコンとリンク設定はまだ）

--- footer.php
+++ footer.php
@@ -8,10 +8,44 @@
 ?>
 	<footer class="site-footer" role="contentinfo">
 		<div class="wrapper wrapper-footer ">
+			<div id="wrapper-footer-contents-list">
+				<div class="footer-contents-list">
+					<a id="footer-logo" href=""></a>
+				</div>
+				<div class="footer-contents-list">
+					<div class="title">ABOUT</div>
+					<ul class="items">
+						<li><a href="">このサイトについて</a></li>
+						<li><a href="">プロフィール</a></li>
+					</ul>
+				</div>
+				<div class="footer-contents-list">
+					<div class="title">BLOG</div>
+					<ul class="items">
+						<li><a href="">線形代数入門</a></li>
+						<li><a href="">豆知識</a></li>
+						<li><a href="">西国三十三ヶ所巡礼</a></li>
+						<li><a href="">Web開発</a></li>
+						<li><a href="">おぐえもんの日記</a></li>
+					</ul>
+				</div>
+				<div class="footer-contents-list">
+					<div class="title">SERVICE</div>
+					<ul class="items">
+						<li><a href="">行列式&逆行列式計算機</a></li>
+						<li><a href="">簡単！手取り給料計算機</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="footer-contents-other-list">
+				<a href="">お問い合わせ</a>
+				・
+				<a href="">サイトマップ</a>
+				・
+				<a href="">利用上の注意点</a>
+			</div>
 		</div><!-- .wrapper .wrapper-footer -->
-		<div class="wrapper wrapper-copy">
-			<p id="copyright">Copyright © 2016-2018 おぐえもん All Rights Reserved.</p>
-		</div><!-- .wrapper .wrapper-copy -->
+		<div id="copyright">Copyright © 2016-2018 おぐえもん All Rights Reserved.</div>
 	</footer><!-- .site-footer -->
 </div><!-- end #container -->
 
