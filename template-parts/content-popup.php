352e158cf5877e0dbfb7cf5d1a2f005acb89802b
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2019-11-21 23:47:39 +0900

    ポップアップの文言を変更

--- template-parts/content-popup.php
+++ template-parts/content-popup.php
@@ -9,10 +9,10 @@
     <div id="popup-wrapper">
         <div class="title">みんなで線形代数を勉強しよう！</div>
         <div class="description">
-        	線形代数のLINEオープンチャットをOPEN！<br>
+        	250人を突破した線形代数のLINEオープンチャット！<br>
         	匿名OK！疑問や発見を語り合い理解を深めよう。
         </div>
     </div>
-    <div id="popup-open">覗いてみる</div>
+    <div id="popup-open">のぞいてみる</div>
     <div id="popup-close">閉じる</div>
 </div>
