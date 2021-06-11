8e218b4556534c3287152e27fd56811769ef2578
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2021-06-11 09:19:54 +0900

    ポップアップのデザインを変更

--- template-parts/content-popup.php
+++ template-parts/content-popup.php
@@ -6,13 +6,15 @@
 ?>
 
 <div id="popup-box">
-    <div class="wrapper popup-content-wrapper">
-        <a id="popup-open" class="popup-info">
-            <div class="title"></div>
-            <div class="description"></div>
-        </a>
-        <a id="popup-close" class="close-btn">
-            <em class="close-icon"></em>
-        </a>
-    </div>
+    <div class="woman-body"></div>
+    <div class="woman-hand"></div>
+    <div class="popup-header"></div>
+    <img class="thumnail" />
+    <a id="popup-open" class="popup-info">
+        <div class="title"></div>
+        <div class="description"></div>
+    </a>
+    <a id="popup-close" class="close-btn">
+        <em class="close-icon"></em>
+    </a>
 </div>
