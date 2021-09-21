5003e5a94a8f3d9619393ce67043bed7445a80a9
Ogura Katsuya <k-ogura@ist.osaka-u.ac.jp>
2021-09-22 08:48:48 +0900

    Amazonアフィリエイトリンクボックスを追加

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
  * @package oguemon
  */
 
+include_once(get_template_directory() . '/func/AwsV4.php');
+
 /**
  * Word Pressに関する初期設定
  */
@@ -417,6 +419,88 @@ function output_textbook_link_box() {
 EOM;
 }
 add_shortcode('textbook', 'output_textbook_link_box');
+
+function output_amzn_link_box($atts) {
+    extract(shortcode_atts([
+        'asin' => '',
+	], $atts));
+
+	$product  = json_decode(get_amazon_product_info($asin));
+	$url      = $product->ItemsResult->Items[0]->DetailPageURL;
+	$imgurl   = $product->ItemsResult->Items[0]->Images->Primary->Large->URL;
+	$title    = $product->ItemsResult->Items[0]->ItemInfo->Title->DisplayValue;
+	$price    = number_format($product->ItemsResult->Items[0]->Offers->Listings[0]->Price->Amount);
+	$get_date = date('Y.m.d');
+
+	return <<<EOM
+	<a class="amzn-box" href="$url" target="_blank">
+		<div class="img-info">
+			<img src="$imgurl" >
+		</div>
+		<div class="text-info">
+			<div class="product-title">$title</div>
+			<div class="product-price"><span class="price">$price</span><span class="unit">円</span><span class="description">（$get_date 現在）</span></div>
+			<div class="product-link">Amazonで購入する</div>
+		</div>
+	</a>
+EOM;
+}
+add_shortcode('amzn', 'output_amzn_link_box');
+
+function get_amazon_product_info ($item_id) {
+
+	$serviceName = "ProductAdvertisingAPI";
+	$region      = "us-west-2";
+	$accessKey   = "AKIAIFFJBJKEUTMVS52Q";
+	$secretKey   = "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";
+	$payload = '{'
+			 . ' "ItemIds": ["' . $item_id . '"],'
+			 . ' "Resources": ['
+			 . '  "Images.Primary.Large",'
+			 . '  "ItemInfo.Title",'
+			 . '  "Offers.Listings.Price"'
+			 . ' ],'
+			 . ' "PartnerTag": "oguemon-22",'
+			 . ' "PartnerType": "Associates",'
+			 . ' "Marketplace": "www.amazon.co.jp"'
+			 . '}';
+	$host="webservices.amazon.co.jp";
+	$uriPath="/paapi5/getitems";
+	$awsv4 = new AwsV4 ($accessKey, $secretKey);
+	$awsv4->setRegionName($region);
+	$awsv4->setServiceName($serviceName);
+	$awsv4->setPath ($uriPath);
+	$awsv4->setPayload ($payload);
+	$awsv4->setRequestMethod ("POST");
+	$awsv4->addHeader ('content-encoding', 'amz-1.0');
+	$awsv4->addHeader ('content-type', 'application/json; charset=utf-8');
+	$awsv4->addHeader ('host', $host);
+	$awsv4->addHeader ('x-amz-target', 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems');
+	$headers = $awsv4->getHeaders ();
+	$headerString = "";
+	foreach ( $headers as $key => $value ) {
+		$headerString .= $key . ': ' . $value . "\r\n";
+	}
+	$params = array (
+			'http' => array (
+				'header' => $headerString,
+				'method' => 'POST',
+				'content' => $payload
+			)
+		);
+	$stream = stream_context_create ( $params );
+
+	$fp = @fopen ( 'https://'.$host.$uriPath, 'rb', false, $stream );
+
+	if (! $fp) {
+		throw new Exception ( "Exception Occured" );
+	}
+	$response = @stream_get_contents ( $fp );
+	if ($response === false) {
+		throw new Exception ( "Exception Occured" );
+	}
+	return $response;
+}
 
 // GETパラメータとして使用できるnameを追加（ABテスト用）
 /*
