04f84936628b73d3698f443f3b27a4b0984caa41
Ogura Katsuya <contact@oguemon.com>
2022-04-09 11:16:27 +0900

    gulpを廃止してwebpackに統合

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-module.exports = {
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const config = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
     mode: 'development',
 
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: './ts/main.ts',
+    entry: {
+      scss_common: './sass/common.scss',
+      scss_edit: './sass/editor-style.scss',
+      scss_embed: './sass/embed.scss',
+      ts: './ts/main.ts',
+    },
     output: {
-        //  出力ファイルのディレクトリ名
-        path: __dirname + '/js',
-        // 出力ファイル名
-        filename: "oguemon.js"
+        path: __dirname,
+        filename: (pathData) => {
+          return pathData.chunk.name === 'ts' ? 'js/oguemon.js' : '[name].js'
+        },
     },
     module: {
       rules: [
@@ -19,8 +27,37 @@ module.exports = {
           // TypeScript をコンパイルする
           use: 'ts-loader',
         },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+              },
+            },
+            'sass-loader',
+          ],
+        },
       ],
     },
+    plugins: [
+      new RemoveEmptyScriptsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: (pathData) => {
+          switch (pathData.chunk.name) {
+            case 'scss_common':
+              return './css/common.css'
+            case 'scss_edit':
+              return './css/editor-style.css'
+            case 'scss_embed':
+              return './css/embed.css'
+          }
+          return './css/[name].css'
+        },
+      }),
+    ],
     // import 文で .ts ファイルを解決するため
     // これを定義しないと import 文で拡張子を書く必要が生まれる。
     // フロントエンドの開発では拡張子を省略することが多いので、
@@ -35,3 +72,11 @@ module.exports = {
     externals: {
     },
 };
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map'
+  }
+
+  return config
+}
